1cf19288f1ca7cb7458033aeddbcd63b65e7ad10
dopamiini <ttkorkki3@gmail.com>
2026-09-20 20:53:51 +0300

    Added registration, login, and logout functionality.

--- app.py
+++ app.py
@@ -1,7 +1,79 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+import config
+import db
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return "Placeholder for Recepio a food recipe app for the course TiKaWe."
+    message = session.pop("message", None)
+    message_type = session.pop("message_type", None)
+    return render_template("index.html", message=message, message_type=message_type)
+
+@app.route("/register")
+def register():
+    message = session.pop("message", None)
+    message_type = session.pop("message_type", None)
+    return render_template("register.html", message=message, message_type=message_type)
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+
+    if password1 != password2:
+        session["message"] = "Error: Passwords are not the same!"
+        session["message_type"] = "error"
+        return redirect("/register")
+    
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        session["message"] = "Error: Username already taken!"
+        session["message_type"] = "error"
+        return redirect("/register")
+
+    session["message"] = "Account created successfully!"
+    session["message_type"] = "success"
+    return redirect("/")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        message = session.pop("message", None)
+        message_type = session.pop("message_type", None)
+        return render_template("login.html", message=message, message_type=message_type)
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        sql = "SELECT password_hash FROM users WHERE username = ?"
+        rows = db.query(sql, [username])
+
+        if len(rows) != 1:
+            session["message"] = "Error: Incorrect username or password!"
+            session["message_type"] = "error"
+            return redirect("/login")
+
+        password_hash = rows[0]["password_hash"]
+
+        if check_password_hash(password_hash, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            session["message"] = "Error: Incorrect username or password!"
+            session["message_type"] = "error"
+            return redirect("/login")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
